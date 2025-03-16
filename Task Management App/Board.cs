705bcc7eb6281751ea8f4d78197693e47de1e201
Matvey <matvey.m.920@gmail.com>
2025-03-16 23:25:10 +0200

    Added main classes of the program (Task, TaskList, Board)

---
+++ Task Management App/Board.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_App
+{
+    public class Board
+    {
+        public string Name { get; set; }
+        public List<TaskList> Lists { get; private set; }
+
+        public Board(string name)
+        {
+            Name = name;
+            Lists = new List<TaskList>();
+        }
+
+        public void AddList(string title)
+        {
+            Lists.Add(new TaskList(title));
+        }
+
+        public void RemoveList(string title)
+        {
+            TaskList list = Lists.FirstOrDefault(l => l.Title == title);
+            if (list != null)
+            {
+                Lists.Remove(list);
+            }
+        }
+
+        public TaskList GetList(string title)
+        {
+            return Lists.FirstOrDefault(l => l.Title == title);
+        }
+    }
+}
---
+++ Task Management App/TaskList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_App
+{
+    public class TaskList
+    {
+        public string Title { get; set; }
+        public List<Task> Tasks { get; private set; }
+
+        public TaskList(string name)
+        {
+            Title = name;
+            Tasks = new List<Task>();
+        }
+
+        public void AddTask(string title, string description, TaskStatus status, DateTime dueDate)
+        {
+            Tasks.Add(new Task(title, description, status, dueDate));
+        }
+
+        public void RemoveTask(int id)
+        {
+            Task task = Tasks.FirstOrDefault(t => t.Id == id);
+            if (task != null)
+            {
+                Tasks.Remove(task);
+            }
+        }
+    }
+}
